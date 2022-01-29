1f64c0b83a0e86933fb04826bc3dda217841a38a
lincoln <lincoln-md@hotmail.com>
2022-01-29 20:14:17 -0300

    trabalhando no css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React from 'react'
+import { ReactSVG } from 'react-svg'
+
 import logo from './images/logo.svg';
 import titles from './images/titles.png';
 import marvel from './images/marvel.svg';
 import playstation from './images/ps.svg';
 import hero from './images/hero.png';
+import facebook from './images/icon-facebook.svg';
+import instagram from './images/icon-instagram.svg';
+import youtube from './images/icon-youtube.svg';
+import twitter from './images/icon-twitter.svg';
+
 import './styles/styles.scss'
 
 export default function App() {
@@ -13,17 +21,22 @@ export default function App() {
         <nav className="navbar">
           <div className="nav-menu">
             <div className="logo">
-              <img src={logo} alt="logo"/>
+              <img src={logo} alt="logo" />
             </div>
             <ul className="nav-list">
-              <li><a href="#" className="Home"></a></li>
-              <li><a href="#" className="Story"></a></li>
-              <li><a href="#" className="Wallpapers"></a></li>
-              <li><a href="#" className="#BeYourself"></a></li>
+              <li><a href="#" className="nav-link">Home</a></li>
+              <li><a href="#" className="nav-link">Story</a></li>
+              <li><a href="#" className="nav-link">Wallpapers</a></li>
+              <li><a href="#" className="nav-link">#BeYourself</a></li>
             </ul>
           </div>
 
-          <div className="nav-social"></div>
+          <div className="nav-social">
+            <ReactSVG src={facebook} alt="facebook" />
+            <ReactSVG src={instagram} alt="instagram" />
+            <ReactSVG src={youtube} alt="youtube" />
+            <ReactSVG src={twitter} alt="twitter" />
+          </div>
         </nav>
       </header>
 
@@ -31,7 +44,7 @@ export default function App() {
         <div className="container-item-1">
 
           <div>
-            <img src={titles} alt="titles"/>
+            <img src={titles} alt="titles" />
           </div>
 
           <p>
@@ -41,17 +54,19 @@ export default function App() {
             Ultrices facilisis nunc massa diam, quis mi imperdiet.
           </p>
 
-          <div className="btn">Pre-order now</div>
-          <div className="btn-secondary">Watch the teaser</div>
+          <div className='container-buttons'>
+            <div className="btn">Pre-order now</div>
+            <div className="btn-secondary">Watch the teaser</div>
+          </div>
 
           <div>
-            <img src={playstation} alt="playstation"/>
-            <img src={marvel} alt="marvel"/>
+            <img src={playstation} alt="playstation" />
+            <img src={marvel} alt="marvel" />
           </div>
         </div>
 
         <div className="container-item-2">
-          <img src={hero} alt="hero"/>
+          <img src={hero} alt="hero" className='spider-man' />
         </div>
       </section>
     </main>
